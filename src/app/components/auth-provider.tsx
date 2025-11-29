cf29a7258b9b27ed687ee1afcd1017d715ab4ac9
Gideon Adeti <gideonadeti0@gmail.com>
2025-11-29 15:04:45 +0000

    feat: add AuthRefreshContext to manage token refresh state in AuthProvider

--- src/app/components/auth-provider.tsx
+++ src/app/components/auth-provider.tsx
@@ -1,17 +1,38 @@
 "use client";
 
-import { useEffect } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
 
 import useAccessToken from "../hooks/use-access-token";
 import useUser from "../hooks/use-user";
 import { refreshAccessToken } from "../utils/auth-query-functions";
 
+interface AuthRefreshContextType {
+  isRefreshing: boolean;
+}
+
+const AuthRefreshContext = createContext<AuthRefreshContextType | undefined>(
+  undefined
+);
+
+export const useAuthRefresh = () => {
+  const context = useContext(AuthRefreshContext);
+
+  if (context === undefined) {
+    throw new Error("useAuthRefresh must be used within AuthProvider");
+  }
+
+  return context;
+};
+
 const AuthProvider = ({ children }: { children: React.ReactNode }) => {
   const { setAccessToken, clearAccessToken } = useAccessToken();
   const { setUser, clearUser } = useUser();
+  const [isRefreshing, setIsRefreshing] = useState(true);
 
   useEffect(() => {
     (async () => {
+      setIsRefreshing(true);
+
       try {
         const { accessToken, user } = await refreshAccessToken();
 
@@ -21,11 +42,17 @@ const AuthProvider = ({ children }: { children: React.ReactNode }) => {
         // If refresh fails, clear auth state (user is not authenticated)
         clearAccessToken();
         clearUser();
+      } finally {
+        setIsRefreshing(false);
       }
     })();
   }, [setAccessToken, setUser, clearAccessToken, clearUser]);
 
-  return <>{children}</>;
+  return (
+    <AuthRefreshContext.Provider value={{ isRefreshing }}>
+      {children}
+    </AuthRefreshContext.Provider>
+  );
 };
 
 export default AuthProvider;
