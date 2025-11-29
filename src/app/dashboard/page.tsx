db0d434197683d5e0058f89fc74c81aa558f4c9f
Gideon Adeti <gideonadeti0@gmail.com>
2025-11-29 15:11:19 +0000

    feat: add loading state during token refresh in dashboard page

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,10 +1,16 @@
 "use client";
 
+import { useRefreshAccessToken } from "../components/auth-provider";
 import useUser from "../hooks/use-user";
 
 const Page = () => {
+  const { isRefreshing } = useRefreshAccessToken();
   const { user } = useUser();
   const firstName = user?.name.split(" ")[0];
+
+  if (isRefreshing) {
+    return <div>Loading...</div>;
+  }
 
   return <div>Welcome, {firstName}</div>;
 };
