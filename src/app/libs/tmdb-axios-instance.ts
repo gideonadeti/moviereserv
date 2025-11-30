34385d05bf683f88eb3a658ee97807980b666248
Gideon Adeti <gideonadeti0@gmail.com>
2025-11-30 04:56:18 +0000

    feat: create TMDB Axios instance for API requests with authorization headers

---
+++ src/app/libs/tmdb-axios-instance.ts
@@ -0,0 +1,10 @@
+import axios from "axios";
+
+const tmdbAxiosInstance = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_TMDB_BASE_URL,
+  headers: {
+    Authorization: `Bearer ${process.env.NEXT_PUBLIC_TMDB_BEARER_TOKEN}`,
+  },
+});
+
+export default tmdbAxiosInstance;
