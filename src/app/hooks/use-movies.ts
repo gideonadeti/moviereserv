74a84bd7654d9844226b0fe5e8c3a7b80603ddc1
Gideon Adeti <gideonadeti0@gmail.com>
2025-11-30 04:58:38 +0000

    feat: create custom hook useMovies to fetch and manage movie data with error handling

---
+++ src/app/hooks/use-movies.ts
@@ -0,0 +1,31 @@
+import { useQuery } from "@tanstack/react-query";
+import type { AxiosError } from "axios";
+import { useEffect } from "react";
+import { toast } from "sonner";
+
+import type { Movie } from "../types/movie";
+import { fetchMovies } from "../utils/movies-query-functions";
+
+const useMovies = () => {
+  const moviesQuery = useQuery<Movie[], AxiosError<{ message: string }>>({
+    queryKey: ["movies"],
+    queryFn: async () => {
+      return fetchMovies();
+    },
+  });
+
+  useEffect(() => {
+    if (moviesQuery.isError) {
+      const message =
+        moviesQuery.error?.response?.data?.message || "Failed to fetch movies";
+
+      toast.error(message, { id: "fetch-movies-error" });
+    }
+  }, [moviesQuery.error?.response?.data, moviesQuery.isError]);
+
+  return {
+    moviesQuery,
+  };
+};
+
+export default useMovies;
