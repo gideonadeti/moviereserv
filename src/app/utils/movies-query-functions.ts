de1c1ef2aa00634914744ea5a592f1caadec8bd7
Gideon Adeti <gideonadeti0@gmail.com>
2025-11-30 05:22:09 +0000

    feat: update fetchMovies function to use dynamic account ID and add fetchGenres function for genre retrieval

--- src/app/utils/movies-query-functions.ts
+++ src/app/utils/movies-query-functions.ts
@@ -7,10 +7,13 @@ interface MoviesResponse {
 }
 
 export const fetchMovies = async () => {
+  const accountId = process.env.NEXT_PUBLIC_TMDB_ACCOUNT_ID;
+  const prefix = `/account/${accountId}/favorite/movies`;
+
   try {
     // Start with page 1 to get total pages
     const firstPageResponse = await axios.get<MoviesResponse>(
-      "/favorite/movies?page=1"
+      `${prefix}?page=1`
     );
 
     const firstPage = firstPageResponse.data;
@@ -21,9 +24,7 @@ export const fetchMovies = async () => {
       const pagePromises = [];
 
       for (let page = 2; page <= firstPage.total_pages; page++) {
-        pagePromises.push(
-          axios.get<MoviesResponse>(`/favorite/movies?page=${page}`)
-        );
+        pagePromises.push(axios.get<MoviesResponse>(`${prefix}?page=${page}`));
       }
 
       const remainingResponses = await Promise.all(pagePromises);
@@ -38,6 +39,17 @@ export const fetchMovies = async () => {
   } catch (error) {
     console.error("Error from `fetchMovies`:", error);
 
+    throw error;
+  }
+};
+
+export const fetchGenres = async () => {
+  try {
+    const response = await axios.get("/genre/movie/list");
+
+    return response.data.genres;
+  } catch (error) {
+    console.error("Error from `fetchGenres`:", error);
     throw error;
   }
 };
