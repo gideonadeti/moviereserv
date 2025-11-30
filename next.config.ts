667d746d23aa8f7e63a97b39113219923a6b4d6a
Gideon Adeti <gideonadeti0@gmail.com>
2025-11-30 12:04:55 +0000

    feat: add remote image pattern for TMDB in Next.js configuration

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,14 @@ const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
   cacheComponents: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "image.tmdb.org",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
