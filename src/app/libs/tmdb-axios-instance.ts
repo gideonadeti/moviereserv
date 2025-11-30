5cdf61c8b4e52ef01e3df5eae1f1b31f3c880bda
Gideon Adeti <gideonadeti0@gmail.com>
2025-11-30 08:44:55 +0000

    fix: update TMDB base URL environment variable for correct API integration

--- src/app/libs/tmdb-axios-instance.ts
+++ src/app/libs/tmdb-axios-instance.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const tmdbAxiosInstance = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_TMDB_BASE_URL,
+  baseURL: process.env.NEXT_PUBLIC_TMDB_API_BASE_URL,
   headers: {
     Authorization: `Bearer ${process.env.NEXT_PUBLIC_TMDB_BEARER_TOKEN}`,
   },
