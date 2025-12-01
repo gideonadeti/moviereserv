705aa6a38069c84423ca09c656b0122d6edbb299
Gideon Adeti <gideonadeti0@gmail.com>
2025-12-01 11:16:59 +0000

    feat: introduce useMoviesPagination hook for managing movie pagination state

---
+++ src/app/movies/use-movies-pagination.ts
@@ -0,0 +1,19 @@
+import { useCallback, useState } from "react";
+
+export const useMoviesPagination = (pageSize: number) => {
+  const [displayedCount, setDisplayedCount] = useState(pageSize);
+
+  const reset = useCallback(() => {
+    setDisplayedCount(pageSize);
+  }, [pageSize]);
+
+  const loadMore = useCallback(() => {
+    setDisplayedCount((prev) => prev + pageSize);
+  }, [pageSize]);
+
+  return {
+    displayedCount,
+    reset,
+    loadMore,
+  };
+};
