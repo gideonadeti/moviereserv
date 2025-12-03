fafe8445648841bc0a08169f37b638623390e147
Gideon Adeti <gideonadeti0@gmail.com>
2025-12-03 13:34:24 +0000

    refactor: change startTime and endTime properties in Showtime interface from Date to string for improved flexibility

--- src/app/types/showtime.ts
+++ src/app/types/showtime.ts
@@ -2,8 +2,8 @@ import type { Auditorium } from "./auditorium";
 
 export interface Showtime {
   id: string;
-  startTime: Date;
-  endTime: Date;
+  startTime: string;
+  endTime: string;
   price: number;
   tmdbMovieId: number;
   numberOfReservations: number;
