bff1f8c865b6cf843f83f418019b065ee95f3f11
Gideon Adeti <gideonadeti0@gmail.com>
2025-12-03 19:30:52 +0000

    feat: create CreateReservationDialog component for managing seat reservations and payment processing

---
+++ src/app/components/create-reservation-dialog.tsx
@@ -0,0 +1,106 @@
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useRef } from "react";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import useShowtimes from "../hooks/use-showtimes";
+import CustomDialogFooter from "./custom-dialog-footer";
+
+interface CreateReservationDialogProps {
+  open: boolean;
+  price: number;
+  onOpenChange: (open: boolean) => void;
+}
+
+export const reservationSchema = z.object({
+  seatIds: z.array(z.string()).min(1),
+  amountPaid: z.coerce.number<number>().min(0),
+});
+
+const CreateReservationDialog = ({
+  open,
+  price,
+  onOpenChange,
+}: CreateReservationDialogProps) => {
+  const submitButtonRef = useRef<HTMLButtonElement>(null);
+  const { createReservationMutation } = useShowtimes();
+  const createReservationSchema = reservationSchema.refine(
+    (data) => data.amountPaid >= data.seatIds.length * price,
+    {
+      message:
+        "Amount paid must be greater than or equal to the number of seats times the price",
+      path: ["amountPaid"],
+    }
+  );
+
+  const form = useForm<z.infer<typeof createReservationSchema>>({
+    resolver: zodResolver(createReservationSchema),
+    defaultValues: {
+      seatIds: [],
+      amountPaid: 0,
+    },
+  });
+
+  const onSubmit = (formValues: z.infer<typeof createReservationSchema>) => {
+    createReservationMutation.mutate({ formValues });
+  };
+
+  return (
+    <Dialog open={open} onOpenChange={onOpenChange}>
+      <DialogContent
+        showCloseButton={false}
+        onInteractOutside={(e) => e.preventDefault()}
+        onEscapeKeyDown={(e) => e.preventDefault()}
+      >
+        <DialogHeader>
+          <DialogTitle>Make Reservation</DialogTitle>
+          <DialogDescription>
+            Select the seats you want to reserve and the amount you want to pay.
+          </DialogDescription>
+        </DialogHeader>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+            <FormField
+              control={form.control}
+              name="amountPaid"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Amount Paid</FormLabel>
+                  <FormControl>
+                    <Input placeholder="0.00" type="number" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <button type="submit" className="hidden" ref={submitButtonRef} />
+          </form>
+        </Form>
+        <CustomDialogFooter
+          isPending={createReservationMutation.isPending}
+          disabled={!form.formState.isDirty}
+          handleCancel={() => onOpenChange(false)}
+          handleSubmit={() => submitButtonRef.current?.click()}
+        />
+      </DialogContent>
+    </Dialog>
+  );
+};
+
+export default CreateReservationDialog;
