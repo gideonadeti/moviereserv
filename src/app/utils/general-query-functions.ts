a80e023b21ed6df1d9a1d440c9352dcfc932dff0
Gideon Adeti <gideonadeti0@gmail.com>
2025-12-03 19:31:27 +0000

    feat: implement createReservation function to handle reservation submissions

--- src/app/utils/general-query-functions.ts
+++ src/app/utils/general-query-functions.ts
@@ -1,3 +1,5 @@
+import type { z } from "zod";
+import type { reservationSchema } from "../components/create-reservation-dialog";
 import axios from "../libs/axios-instance";
 
 export const fetchShowtimes = async () => {
@@ -7,6 +9,20 @@ export const fetchShowtimes = async () => {
     return response.data;
   } catch (error) {
     console.error("Error from `fetchShowtimes`:", error);
+
+    throw error;
+  }
+};
+
+export const createReservation = async (
+  formValues: z.infer<typeof reservationSchema>
+) => {
+  try {
+    const response = await axios.post("/reservations", formValues);
+
+    return response.data;
+  } catch (error) {
+    console.error("Error from `createReservation`:", error);
 
     throw error;
   }
