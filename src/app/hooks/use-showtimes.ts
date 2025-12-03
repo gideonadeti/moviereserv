d9f844560d5ebfaba415202c09cf081d5350280e
Gideon Adeti <gideonadeti0@gmail.com>
2025-12-03 19:31:39 +0000

    feat: add createReservation mutation to useShowtimes hook for handling reservation submissions

--- src/app/hooks/use-showtimes.ts
+++ src/app/hooks/use-showtimes.ts
@@ -1,10 +1,14 @@
-import { useQuery } from "@tanstack/react-query";
+import { useMutation, useQuery } from "@tanstack/react-query";
 import type { AxiosError } from "axios";
 import { useEffect } from "react";
 import { toast } from "sonner";
-
-import type { Showtime } from "../types/showtime";
-import { fetchShowtimes } from "../utils/general-query-functions";
+import type z from "zod";
+import type { reservationSchema } from "../components/create-reservation-dialog";
+import type { Reservation, Showtime } from "../types/showtime";
+import {
+  createReservation,
+  fetchShowtimes,
+} from "../utils/general-query-functions";
 
 const useShowtimes = () => {
   const showtimesQuery = useQuery<Showtime[], AxiosError<{ message: string }>>({
@@ -24,8 +28,19 @@ const useShowtimes = () => {
     }
   }, [showtimesQuery.error?.response?.data, showtimesQuery.isError]);
 
+  const createReservationMutation = useMutation<
+    Reservation,
+    AxiosError<{ message: string }>,
+    { formValues: z.infer<typeof reservationSchema> }
+  >({
+    mutationFn: async ({ formValues }) => {
+      return createReservation(formValues);
+    },
+  });
+
   return {
     showtimesQuery,
+    createReservationMutation,
   };
 };
 
